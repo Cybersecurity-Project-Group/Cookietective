204bc23cc56e213e2e7661fdc92d318a4e63cd35
Trevor Chan <trevchan@bu.edu>
2023-03-17 20:48:05 -0400

    Added function for checking the packets of capture

--- dnsscan.py
+++ dnsscan.py
@@ -1,6 +1,16 @@
 import scapy.all as scapy
 
 
+def parseDNS(capture):
+
+    # Check if any DNS packets were scanned in capture
+    if (len(capture) < 1):
+        print("No DNS requests found")
+        return []
+    
+    for packet in capture:
+        print(i[scapy.DNS.an[0]].show())
+        
 # Listens to traffic for DNS traffic (udp port 53) for 5 seconds then prints summary
 capture = scapy.sniff(filter="udp port 53", timeout=5)
-capture.summary()
+parseDNS(capture)
