c957132f392f4204f4398e5ab557cd7d0a837566
Trevor Chan <trevchan@bu.edu>
2023-03-18 14:16:52 -0400

    Made it so that it now ONLY scans for CNAME packets, saves them to a list, and prints out all found packets

--- dnsscan.py
+++ dnsscan.py
@@ -1,32 +1,30 @@
 import scapy.all as scapy
 
+listCNAMES = []
+
 # Function to be run everytime sniffer encounters a DNS packet
 def parseDNS(packet):
 
     # Check if packet contains Transport layer UDP and Application layer DNS
     if (packet and packet.haslayer('DNS') and packet.haslayer('UDP')):
-        ip = packet['IP']
         udp = packet['UDP']
         dns = packet['DNS']
 
-        # Check if packet is DNS request (destination binded to DNS port)
-        if int(udp.dport) == 53:
-            qtype_field = dns.qd.get_field('qtype')
-            print("\n### DNS Request ###\n")
-            print("Qname:", dns.qd.qname)
-            print("Qtype:", qtype_field.i2repr(dns.qd, dns.qd.qtype))
-
-
         # Check if packet is DNS response (source binded to DNS port)
-        elif int(udp.sport) == 53:
+        if (int(udp.sport) == 53):
             # Iterate through all DNS resource records
             for i in range(dns.ancount):
                 dnsrr = dns.an[i]
                 type_field = dnsrr.get_field('type')
-                print("\n### DNS Response ###\n")
-                print("Name:", dnsrr.rrname)
-                print("Type:", type_field.i2repr(dnsrr, dnsrr.type))
-                print("Data:", dnsrr.rdata)
+
+                # If DNS entry is of type CNAME, store it in listCNAMES
+                if (type_field.i2repr(dnsrr, dnsrr.type) == 'CNAME'):
+                    listCNAMES.append(dnsrr)
         
 # Listens to traffic for DNS traffic (udp port 53) for 5 seconds then prints summary
 scapy.sniff(filter="udp port 53", timeout=5, prn=parseDNS)
+
+# Iterate through all CNAME packets and list their name, alias pair
+for i, packet in enumerate(listCNAMES):
+    type_field = packet.get_field('type')
+    print("CNAME Packet", i, ":", packet.rrname, "->", packet.rdata)
