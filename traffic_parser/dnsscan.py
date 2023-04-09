0a9edb2375557d46fda2e5c05cb8b285a409f282
Trevor Chan <trevchan@bu.edu>
2023-04-08 23:37:41 -0400

    dnsscanner: Added list of IP addresses from A-type DNS packets to the class

--- traffic_parser/dnsscan.py
+++ traffic_parser/dnsscan.py
@@ -7,6 +7,7 @@ listCNAMES = []
 
 class CNAME_packet():
     def __init__(self, packet, A, ip):
+    def __init__(self, packet, A, ip):
         self.packet = packet
         self.has_A = A
         self.domain = packet.rrname
@@ -15,6 +16,7 @@ class CNAME_packet():
             self.ip = None
         else:
             self.ip = ip
+        self.ip = ip
         
 
 # Function to be run everytime sniffer encounters a DNS packet
@@ -53,6 +55,30 @@ def parseDNS(packet):
 def main():
     scapy.load_layer("http")
     scapy.load_layer("tls")
+            listCNAMES.append(CNAME_packet(dns.an[CNAME_index], has_Atype, ip))
+            # print(ip)
+                    
+
+
+def parseHTTP(packet):
+    # Check if on TLS layer (since HTTP and HTTPS requests are only on TCP)
+    if (packet.haslayer('TCP')):
+        if (packet.haslayer(http.HTTPRequest) or packet.haslayer(http.HTTPResponse) or packet.haslayer('SSL_TLS')):
+            # if (packet.haslayer(scapy.Raw)):
+            #     keys = ["Domain", "Set-Cookie"]
+                # if any(key in packet[scapy.Raw].load for key in keys):
+                #     print(packet[scapy.Raw].load)
+            if (packet.haslayer(http.HTTPRequest)):
+                print(packet[http.HTTPRequest].fields.get('Cookie'))
+            else:
+                print(packet[http.HTTPResponse].fields.get('Cookie'))
+            print(packet.show())
+    # if (packet.haslayer('TCP')):
+    #     if(packet['TCP'].payload != scapy.packet.raw):
+    #         print(packet.iteritems())
+        
+scapy.load_layer("http")
+scapy.load_layer("tls")
 
     # Listens to traffic for DNS traffic (udp port 53) for 15 seconds then prints summary
     scapy.sniff(filter="udp port 53", timeout=15, prn=parseDNS)
