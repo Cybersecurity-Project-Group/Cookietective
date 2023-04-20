a2c9c3abe0f8218cb2eea7b259564fa227eb7f5e
colewentzel <wentzel@bu.edu>
2023-04-20 18:53:35 -0400

    Update sql_func.py
    added all insert functions

--- sql/sql_func.py
+++ sql/sql_func.py
@@ -4,18 +4,21 @@ conn = sqlite3.connect('database.db')
 cur = conn.cursor()
 
 
-''' The table has the following columns
-cur.execute("""CREATE TABLE CNAMEpackets (
-    domainName text,
-    sourceAddress text,
-    CNAMEAlias text,
-    hasAType int
-    )""")
-'''
+# Insert functions
 
 def insertCNAMEpacketsEntry(domainName, sourceAddress, CNAMEAlias, hasAType):
     cur.execute("INSERT INTO CNAMEpackets VALUES (?, ?, ?, ?)", (domainName, sourceAddress, CNAMEAlias, hasAType))
     return
+
+def insertIpEntry(domainName, ip):
+    cur.execute("INSERT INTO ip VALUES (?, ?)", (domainName, ip))
+    return
+
+def insertCookieEntry(domainName, src_ip, domain_setting, httponly, secure):
+    cur.execute("INSERT INTO cookie VALUES (?, ?, ?, ?, ?)", (domainName, src_ip, domain_setting, httponly, secure))
+    return
+
+# Fetch functions
 
 def fetchDomainFromAlias(CNAMEAlias):
     cur.execute("SELECT domainName FROM CNAMEpackets WHERE CNAMEAlias = ?", (CNAMEAlias,))
