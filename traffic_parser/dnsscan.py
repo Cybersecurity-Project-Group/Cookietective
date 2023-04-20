11b5a1fbcd7a7bbbbf448e29d1374fa9867d03eb
Trevor Chan <trevchan@bu.edu>
2023-04-20 19:07:38 -0400

    fixed

--- traffic_parser/dnsscan.py
+++ traffic_parser/dnsscan.py
@@ -4,10 +4,8 @@ listCNAMES = []
 
 # TO DO: 
 # - 
-# - 
 
 class CNAME_packet():
-    def __init__(self, packet, A, ip):
     def __init__(self, packet, A, ip):
         self.packet = packet
         self.has_A = A
@@ -17,7 +15,6 @@ class CNAME_packet():
             self.ip = None
         else:
             self.ip = ip
-        self.ip = ip
         
 
 # Function to be run everytime sniffer encounters a DNS packet
@@ -56,44 +53,13 @@ def parseDNS(packet):
 def main():
     scapy.load_layer("http")
     scapy.load_layer("tls")
-            listCNAMES.append(CNAME_packet(dns.an[CNAME_index], has_Atype, ip))
-            # print(ip)
-                    
 
-
-def parseHTTP(packet):
-    # Check if on TLS layer (since HTTP and HTTPS requests are only on TCP)
-    if (packet.haslayer('TCP')):
-        if (packet.haslayer(http.HTTPRequest) or packet.haslayer(http.HTTPResponse) or packet.haslayer('SSL_TLS')):
-            # if (packet.haslayer(scapy.Raw)):
-            #     keys = ["Domain", "Set-Cookie"]
-                # if any(key in packet[scapy.Raw].load for key in keys):
-                #     print(packet[scapy.Raw].load)
-            if (packet.haslayer(http.HTTPRequest)):
-                print(packet[http.HTTPRequest].fields.get('Cookie'))
-            else:
-                print(packet[http.HTTPResponse].fields.get('Cookie'))
-            print(packet.show())
-    # if (packet.haslayer('TCP')):
-    #     if(packet['TCP'].payload != scapy.packet.raw):
-    #         print(packet.iteritems())
-        
-scapy.load_layer("http")
-scapy.load_layer("tls")
-
-    # Listens to traffic for DNS traffic (udp port 53) for 15 seconds then prints summary
-    scapy.sniff(filter="udp port 53", timeout=15, prn=parseDNS)
     # Listens to traffic for DNS traffic (udp port 53) for 15 seconds then prints summary
     scapy.sniff(filter="udp port 53", timeout=15, prn=parseDNS)
 
     # for packet in scapy.PcapReader('test.pcap'):
     #     parseDNS(packet)
-    # for packet in scapy.PcapReader('test.pcap'):
-    #     parseDNS(packet)
 
-    # # Iterate through all CNAME packets and list their name, alias pair
-    # for i, packet in enumerate(listCNAMES):
-    #     print("CNAME Packet", i, ": Domain:", packet.domain, "CNAME Alias:", packet.cname, "Has A-type", packet.has_A)
     # # Iterate through all CNAME packets and list their name, alias pair
     # for i, packet in enumerate(listCNAMES):
     #     print("CNAME Packet", i, ": Domain:", packet.domain, "CNAME Alias:", packet.cname, "Has A-type", packet.has_A)
