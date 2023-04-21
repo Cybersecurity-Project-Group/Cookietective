97997ad19f8437221fcebb036220b85b06f7261a
Trevor Chan <trevchan@bu.edu>
2023-04-21 13:18:47 -0400

    Added in bash script code that can configure network settings for proxies

--- traffic_parser/test.sh
+++ traffic_parser/test.sh
@@ -3,3 +3,9 @@
 sudo tcpdump -XX -A -G 10 -W 1 -w test.pcap &
 python3 dnsscan.py test.pcap
 # python3 test_mitmproxy.py test.pcap
+
+networksetup -setwebproxy "Wi-Fi" localhost 8080
+networksetup -setsecurewebproxy "Wi-Fi" localhost 8080
+
+networksetup -setwebproxystate "Wi-Fi" off
+networksetup -setsecurewebproxystate "Wi-Fi" off
