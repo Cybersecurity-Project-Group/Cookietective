0d7abf51ee3a561b244a51bd414976ea0966a04c
Trevor Chan <trevchan@bu.edu>
2023-04-21 14:50:23 -0400

    Setting up proxies now works for linux and mac, next up is setting up automating the certificate

--- traffic_parser/test.sh
+++ traffic_parser/test.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # Set up the network settings based on the operating system
+# Install the mitmproxy certificate
+openssl x509 -in mitmproxy-ca-cert.pem -out mitmproxy-ca-cert.crt
+
+# Install certificate and set up proxies for Mac
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Add in the mitmproxy certificate
+    sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain mitmproxy-ca-cert.crt
+    
     networksetup -setwebproxy "Wi-Fi" localhost 8080
     networksetup -setsecurewebproxy "Wi-Fi" localhost 8080
+# Install certificate and set up proxies for Linux
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    gsettings set org.gnome.system.proxy.http host 'my.proxy.com'
-    gsettings set org.gnome.system.proxy.http port 8000
-
-    gsettings set org.gnome.system.proxy.https host 'my.proxy.com'
-    gsettings set org.gnome.system.proxy.https port 8000
-
-    gsettings set org.gnome.system.proxy mode 'manual'
+    certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n "mitmproxy" -i mitmproxy-ca-cert.crt
+    export http_proxy=localhost:8080
+    export https_proxy=localhost:8080
 
 else
     echo "ERROR: Not a supported Operating System"
@@ -21,7 +25,7 @@ fi
 # sudo tcpdump -XX -A -G 10 -W 1 -w test.pcap &
 # python3 dnsscan.py test.pcap
 # python3 test_mitmproxy.py test.pcap
-
+mitmproxy
 sleep 10
 
 
@@ -30,5 +34,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     networksetup -setwebproxystate "Wi-Fi" off
     networksetup -setsecurewebproxystate "Wi-Fi" off
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    gsettings set org.gnome.system.proxy mode 'none'
+    unset http_proxy
+    unset https_proxy
 fi
