0e031175e1322b4be02909f562ed4377597603ca
Trevor Chan <trevchan@bu.edu>
2023-04-22 01:59:26 -0400

    Got test_mitmproxy to get the domain and ip address and started working on cookie parser

--- traffic_parser/test_mitmproxy.py
+++ traffic_parser/test_mitmproxy.py
@@ -1,19 +1,29 @@
 from mitmproxy import http
 
+# Response function that is used whenever mitmproxy encounters an https response message
 def response(flow: http.HTTPFlow) -> None:
     # Check if the response has Set-Cookie headers
-    if "set-cookie" in flow.response.headers:
+    if ("set-cookie" in flow.response.headers):
         # Get the Set-Cookie headers and extract the cookie values
         set_cookie_headers = flow.response.headers.get_all("set-cookie")
         cookies = []
+
+        # Add all found cookie headers into the cookies list
         for set_cookie_header in set_cookie_headers:
             cookies.extend(set_cookie_header.split(";"))
 
+        # If any cookie headers are found, store into a file
         if (cookies):
             # Save the cookies to a file
             with open("cookies.txt", "a") as f:
-                f.write(f"Source domain: {flow.request.pretty_host}\n")
-                f.write(f"Source IP address: {flow.server_conn.ip_address[0]}\n")
-                f.write("\n".join(cookies))
-                f.write("\n\n")
+                f.write("### Cookie ###\n\n")
+                for cookie in cookies:
+                    if (cookie[0] != " "):
+                        f.write(f"Source domain: {flow.request.pretty_host}\n")
+                        f.write(f"Source IP address: {flow.server_conn.ip_address[0]}\n")
+
+                    # f.write("\n".join(cookies))
+                    f.write(cookie)
+                    f.write("\n")
+                f.write("\n\n\n")
 
