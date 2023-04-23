d97f4f81f5baba2cf0f9e153c7fe4d14c97a46f8
Trevor Chan <trevchan@bu.edu>
2023-04-22 23:20:52 -0400

    Modified sql slightly so that all values are unique (and doesn't exit if it fails to insert non-unique value bc there were a lot of duplicates

--- sql/sql_init.py
+++ sql/sql_init.py
@@ -8,22 +8,25 @@ cur.execute("""CREATE TABLE CNAMEpackets (
     domainName text,
     sourceAddress text,
     CNAMEAlias text,
-    hasAType int
+    hasAType int,
+    UNIQUE(domainName, sourceAddress, CNAMEAlias, hasAType)
     )""")
 
 # Initialize the IP table that stores IP packets associated with A-type packets
 cur.execute("""CREATE TABLE ip (
     domainName text NOT NULL,
-    ip text NOT NULL
+    ip text NOT NULL,
+    UNIQUE(domainName, ip)
     )""")
 
 # Initialize the cookie table that stores Set-Cookie information from MITMproxy
 cur.execute("""CREATE TABLE cookie (
     domainName text NOT NULL,
-    src_ip text NOT NULL,
+    sourceAddress text NOT NULL,
     domain_setting text NOT NULL,
     httponly int,
-    secure int
+    secure int,
+    UNIQUE(domainName, sourceAddress, domain_setting, httponly, secure)
     )""")
 
 conn.commit()
