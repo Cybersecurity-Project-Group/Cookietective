d61ec746648ed79baf4a0d81d6a5b64300767cd6
Trevor Chan <trevchan@bu.edu>
2023-04-29 01:57:58 -0400

    Made run.sh snazzy

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NUM_CONTAINERS=2
+NUM_URLS=10
+URLS_PER_CONTAINER=$(( $NUM_URLS/$NUM_CONTAINERS ))
+
+for (( i=0 ; i < $NUM_CONTAINERS ; i++ )); do
+    UDP_PORT=$(( $i + 9000 ))
+    HTTPS_PORT=$(( $i + 9500))
+
+    start=$(( $URLS_PER_CONTAINER*$i ))
+    end=$(( $URLS_PER_CONTAINER*($i+1) -1 ))
+    
+    docker run -p $UDP_PORT:53/udp -p $HTTPS_PORT:8080 snickerdoodle $start $end
+
+done
+
+
+# docker run -p 9000:53/udp 9500:8080 snickerdoodle 0 4
+# docker run -p 9001:53/udp 9501:8080 snickerdoodle 5 8
