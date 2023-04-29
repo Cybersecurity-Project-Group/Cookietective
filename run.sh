852b026327e0f85d2c38f2ad33e08ea096161f22
Trevor Chan <trevchan@bu.edu>
2023-04-29 02:08:24 -0400

    MULTI IS WORKING WOO! Semi-finalized script for automating multi containers

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ NUM_CONTAINERS=2
 NUM_URLS=10
 URLS_PER_CONTAINER=$(( $NUM_URLS/$NUM_CONTAINERS ))
 
+docker build -t snickerdoodle .
+
 for (( i=0 ; i < $NUM_CONTAINERS ; i++ )); do
     UDP_PORT=$(( $i + 9000 ))
     HTTPS_PORT=$(( $i + 9500))
