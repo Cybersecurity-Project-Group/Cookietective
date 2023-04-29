69d25115380d0212c9eed48e307b9fa9c2697639
Andres Garcia <andresfg@bu.edu>
2023-04-29 00:41:40 -0400

    Updated cloakdetector

--- cloakdetector.py
+++ cloakdetector.py
@@ -1,63 +1,59 @@
-import socket
 import sqlite3
-import pandas as pd
 import sys
+import tldextract
+import whois
 from url_checker import url_func
 from sql import sql_func
 
-def is_first_party_domain(domain_name, cname_domain_name):
-    return domain_name.endswith(cname_domain_name)
+def sameOwner(url1, url2):
+    ext1 = tldextract.extract(url1)
+    ext2 = tldextract.extract(url2)
 
-def get_original_ip(domain_name):
+    # check if the extracted top-level domains match
+    if ext1.suffix == ext2.suffix:
+        return True
+
+    # if the top-level domains don't match, use the mostMatching function to check if they belong to the same owner
+    matchCount, matches = mostMatching(url1, url2)
+
+    # if there is only one matching domain and it is the top-level domain, assume they belong to the same owner
+    if matchCount == 1 and matches[0] == ext1.suffix:
+        return True
+
+    # perform a WHOIS lookup for both domains and compare the registrant organization fields
     try:
-        return socket.gethostbyname(domain_name)
+        w1 = whois.whois(url1)
+        w2 = whois.whois(url2)
+        if w1.status == w2.status == None and w1.registrant_org == w2.registrant_org:
+            return True
     except:
-        return None
+        pass
 
-def check_domain(domain_name):
-    conn = sqlite3.connect('database.db')
-    cur = conn.cursor()
+    return False
 
-    # Check for A type records
-    cur.execute("SELECT hasAType FROM CNAMEpackets WHERE domainName = ?", (domain_name,))
-    a_type_records = cur.fetchall()
-    has_a_type_record = False
-    for record in a_type_records:
-        if record[0] == 1:
-            has_a_type_record = True
 
-    # Check for CNAME records
-    cur.execute("SELECT CNAMEAlias FROM CNAMEpackets WHERE domainName = ?", (domain_name,))
-    cname_records = cur.fetchall()
-    has_cname_record = False
-    cname_domain_name = None
-    for record in cname_records:
-        has_cname_record = True
-        cname_domain_name = record[0]
-
-    # Check if CNAME domain is a first-party domain
-    is_first_party = False
-    if has_cname_record:
-        is_first_party = is_first_party_domain(domain_name, cname_domain_name)
-
-    # Check if IP address is an IP address of the original URL
-    original_ip = get_original_ip(domain_name)
-    is_original_ip = False
-    if original_ip:
-        cur.execute("SELECT ip FROM ip WHERE domainName = ?", (domain_name,))
-        ip_records = cur.fetchall()
-        for record in ip_records:
-            if record[0] == original_ip:
-                is_original_ip = True
-
-    conn.commit()
+def hasAType(domainName, dbFile='database.db'):
+    conn = sqlite3.connect(dbFile)
+    c = conn.cursor()
+    c.execute("SELECT hasAType FROM CNAMEpackets WHERE domainName=?", (domainName,))
+    result = c.fetchone()
     conn.close()
+    if result is not None:
+        return result[0]
+    else:
+        return None
+        
 
-    return has_a_type_record, has_cname_record, is_first_party, is_original_ip
+def hasCNAMErecord(domainName, dbFile='database.db'):
+    conn = sqlite3.connect(dbFile)
+    c = conn.cursor()
+    c.execute("SELECT EXISTS(SELECT 1 FROM CNAMEpackets WHERE domainName = ? LIMIT 1)", (domainName,))
+    result = c.fetchone()[0]
+    conn.close()
+    return result
+    
+    
 
-domain_name = input("Enter domain name: ")
-has_a_type_record, has_cname_record, is_first_party, is_original_ip = check_domain(domain_name)
-print("Has A type record:", has_a_type_record)
-print("Has CNAME record:", has_cname_record)
-print("Is CNAME domain a first-party domain:", is_first_party)
-print("Is domain IP address an IP address of the original URL:", is_original_ip)
+
+
+
