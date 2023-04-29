0ea80678612339cd07706ac6d31a38cdb868291c
Trevor Chan <trevchan@bu.edu>
2023-04-29 01:39:59 -0400

    Added in run.sh to run 2 containers at same time

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+NUM_CONTAINERS=2
 NUM_URLS=10
+URLS_PER_CONTAINER=$(( $NUM_URLS/$NUM_CONTAINERS ))
 
-for i in {1..$NUM_URLS}; do
-    echo $i
-done
+# echo $URLS_PER_CONTAINER
+# for (( i=0 ; i < $NUM_CONTAINERS ; i++ )); 
+#     do echo $i
+# done
+
+
+docker run -p 9000:53/udp snickerdoodle 0 4
+docker run -p 9001:53/udp snickerdoodle 5 8
